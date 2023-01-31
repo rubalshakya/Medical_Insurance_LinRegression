e1a22dde44f70f9fc4c626c178d3e35675a0db8c
Rubal Shakya <rubalshakya@gmail.com>
2023-01-31 23:33:10 +0530

    modify the base function with method 'get' to return back to home.html from result.html

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from project_app.utils import MedicalInsurance
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["post","get"])
 def base():
     return render_template("home.html")
 
